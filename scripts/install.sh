ad5080629cf9d08ba4709b082702c160730ec596
Alon Grinberg Dana <alongd@mit.edu>
2022-11-21 07:49:35 +0200

    Modified the Aluf installation script

---
+++ scripts/install.sh
@@ -0,0 +1,39 @@
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+
+source ~/.bashrc
+
+cd /home/$USER
+mkdir Code
+cd Code
+
+# Clone repos
+git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
+git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
+git clone https://github.com/ReactionMechanismGenerator/ARC.git
+
+cd ARC
+conda env create -f environment.yml
+cd ..
+
+# Echo paths to ~/.bashrc
+
+#echo 'export PYTHONPATH=$HOME/Code/RMG-Py/:$PYTHONPATH' >> ~/.bashrc
+#echo 'export PATH=$HOME/Code/RMG-Py/:$PATH ' >> ~/.bashrc
+#echo 'export PYTHONPATH=$HOME/Code/ARC/:$PYTHONPATH' >> ~/.bashrc
+#echo 'export PATH=$HOME/Code/ARC/:$PATH' >> ~/.bashrc
+
+source ~/.bashrc
+
+# Compile RMG
+cd RMG-Py
+conda activate arc_env
+make
+python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
+julia -e 'using Pkg; Pkg.add(PackageSpec(name="ReactionMechanismSimulator",rev="main")); using ReactionMechanismSimulator;'
+
+# Install ARC's requirements.
+cd ../ARC/
+make install-all
+conda deactivate
+
