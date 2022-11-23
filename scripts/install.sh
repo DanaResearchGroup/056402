c1eba6d8db4c60bdefcf8b2df1038df934a05cf9
Alon Grinberg Dana <alongd@mit.edu>
2022-11-23 08:52:38 +0200

    modified install and students bashrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,18 @@
+bash
+
+# reset .bashrc
+cp /home/$USER/.bashrc /home/$USER/.bashrc_backup_1
+rm /home/$USER/.bashrc
+wget /home/$USER https://raw.githubusercontent.com/DanaResearchGroup/056402/main/scripts/students_bashrc
+cp /home/$USER/students_bashrc /home/$USER/.bashrc
+source ~/.bashrc
+
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 
 source ~/.bashrc
+
+conda remove --name arc_env --all -y
 
 cd /home/$USER
 mkdir Code
@@ -10,30 +21,14 @@ cd Code
 # Clone repos
 git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
 git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
-git clone https://github.com/ReactionMechanismGenerator/ARC.git
 
-cd ARC
-conda env create -f environment.yml
-cd ..
-
-# Echo paths to ~/.bashrc
-
-#echo 'export PYTHONPATH=$HOME/Code/RMG-Py/:$PYTHONPATH' >> ~/.bashrc
-#echo 'export PATH=$HOME/Code/RMG-Py/:$PATH ' >> ~/.bashrc
-#echo 'export PYTHONPATH=$HOME/Code/ARC/:$PYTHONPATH' >> ~/.bashrc
-#echo 'export PATH=$HOME/Code/ARC/:$PATH' >> ~/.bashrc
-
-source ~/.bashrc
-
-# Compile RMG
 cd RMG-Py
+conda env create -f environment.yml
+source ~/.bashrc
 conda activate arc_env
 make
 python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
 julia -e 'using Pkg; Pkg.add(PackageSpec(name="ReactionMechanismSimulator",rev="main")); using ReactionMechanismSimulator;'
 
-# Install ARC's requirements.
-#cd ../ARC/
-#make install-all
 conda deactivate
 
